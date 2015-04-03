fd95600cfa2b8f77bfcfb8e6a0fb73540eec45fe
Matt Heath <matt@mattheath.com>
2015-04-04 00:25:38 +0100

    Fix time conversion to ms

--- util/util.go
+++ util/util.go
@@ -19,10 +19,10 @@ func MacAddressToWorkerId(mac string) (uint64, error) {
 
 // CustomTimestamp takes a timestamp and adjusts it to our custom epoch
 func CustomTimestamp(epoch int64, t time.Time) int64 {
-	return t.UnixNano()/1000000 - epoch
+	return TimeToMsInt64(t) - epoch
 }
 
 // TimeToMsInt64 returns the number of ms since the unix epoch as an int64
 func TimeToMsInt64(t time.Time) int64 {
-	return int64(t.UTC().UnixNano() / 1000000)
+	return int64(t.UTC().Unix()*1e3) + int64(t.UTC().Nanosecond()/1e6)
 }
