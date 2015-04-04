a6fe5a5fe28e0806d361fda28a373b9a6221a544
Matt Heath <matt@mattheath.com>
2015-04-04 02:14:15 +0100

    Add tests for timestamps with custom epoch

--- util/util_test.go
+++ util/util_test.go
@@ -16,6 +16,34 @@ func TestMacAddressToWorkerId(t *testing.T) {
 	assert.Equal(t, uint(140972585083926), workerId)
 }
 
+func TestCustomTimestamp(t *testing.T) {
+	// Formatted using the default format string
+	// "2006-01-02 15:04:05.999999999 -0700 MST"
+	testcases := []struct {
+		epoch     int64
+		timestamp string
+		expected  int64
+	}{
+		{0, "2012-01-01 00:00:00.000000000 +0000 UTC", 1325376000000},
+		{1325376000000, "2012-01-01 00:00:00.000000000 +0000 UTC", 0},
+		{1325376000000, "2012-01-01 00:00:00.000000001 +0000 UTC", 0},
+		{1325376000000, "2012-01-01 00:00:00.001000001 +0000 UTC", 1},
+		{1325376000000, "2011-12-31 23:59:59.999999999 +0000 UTC", -1},          // will be floored to .999
+		{1325376000000, "2013-01-01 00:00:00.000000000 +0000 UTC", 31622400000}, // 2012 was a leap year
+		{1325376000000, "2013-01-01 00:00:00.000000000 +0000 UTC", 31622400000},
+		{1325376000000, "2081-09-06 15:47:35 +0000 UTC", 2199023255000},
+		{1325376000000, "2840-04-02 20:16:16.530845939 +0000 UTC", 26137196176530},
+	}
+
+	for _, tc := range testcases {
+		ts, err := time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", tc.timestamp)
+		require.NoError(t, err)
+		ct := CustomTimestamp(tc.epoch, ts)
+		t.Logf("%v\t%v\t%v", tc.epoch, ts, ct)
+		assert.Equal(t, tc.expected, ct)
+	}
+}
+
 func TestTimeToMsInt64(t *testing.T) {
 	// Formatted using the default format string
 	// "2006-01-02 15:04:05.999999999 -0700 MST"
